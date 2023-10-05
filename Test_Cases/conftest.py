62a0e566723ed3dd5b45903627bf3112e447fe6d
jasveerhashtrust <jasveer@hashtrust.in>
2023-10-05 18:35:47 +0530

    new urlssssss

--- Test_Cases/conftest.py
+++ Test_Cases/conftest.py
@@ -1,30 +1,24 @@
 import time
 import pytest
 from selenium import webdriver
-# driver = None
-import shutil
-
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-
-# Search for the Chrome binary in common locations
-chrome_binary = shutil.which(
-    'google-chrome') or shutil.which('chromium-browser')
-
-if chrome_binary:
-    options.binary_location = chrome_binary
-    driver = webdriver.Chrome(options=options)
-else:
-    raise Exception("Chrome binary not found. Please specify the path.")
+from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture(scope="class")
 def setup(request):
     # global driver
-    request.cls.driver = webdriver.Chrome()
-    request.cls.driver.get(
-        'https://fetch.ai/docs/guides/agents/create-a-uagent')
-    request.cls.driver.maximize_window()
+    service = Service(
+        executable_path="/opt/google/chrome/google-chrome")
+    driver = webdriver.Chrome(service=service)
+    driver.get('https://fetch.ai/docs/guides/agents/create-a-uagent')
+    driver.maximize_window()
     time.sleep(2)
     yield
-    request.cls.driver.quit()
+    driver.quit()
+    # request.cls.driver = webdriver.Chrome()
+    # request.cls.driver.get(
+    #     'https://fetch.ai/docs/guides/agents/create-a-uagent')
+    # request.cls.driver.maximize_window()
+    # time.sleep(2)
+    # yield
+    # request.cls.driver.quit()
