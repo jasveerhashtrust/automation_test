e12e9f448fd50f67da6ea5fa44e728e6951d43ed
jasveerhashtrust <jasveer@hashtrust.in>
2023-10-05 17:14:34 +0530

    fix code

--- Test_Cases/conftest.py
+++ Test_Cases/conftest.py
@@ -2,11 +2,19 @@ import time
 import pytest
 from selenium import webdriver
 # driver = None
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(options=options)
+
+
 @pytest.fixture(scope="class")
 def setup(request):
     # global driver
     request.cls.driver = webdriver.Chrome()
-    request.cls.driver.get('https://fetch.ai/docs/guides/agents/create-a-uagent')
+    request.cls.driver.get(
+        'https://fetch.ai/docs/guides/agents/create-a-uagent')
     request.cls.driver.maximize_window()
     time.sleep(2)
     yield
